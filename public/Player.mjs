8b8a743e5b829de0e9b4ef7ca24f91c6e9c67275
remchaf <70279203+remchaf@users.noreply.github.com>
2022-11-02 03:02:06 +0000

    Started working on the project.

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,37 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor({ x, y, score, id }) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
   }
 
   movePlayer(dir, speed) {
+    switch (dir) {
+      case "up":
+        this.y -= speed;
+        break;
+      case "down":
+        this.y += speed;
+        break;
+      case "left":
+        this.x -= speed;
+        break;
+      case "right":
+        this.x += speed;
+        break;
 
+      default:
+        true;
+        break;
+    }
   }
 
-  collision(item) {
-
-  }
+  collision(item) {}
 
   calculateRank(arr) {
-
+    let sorted = arr.sort((p1, p2) => p2.score - p1.score);
+    return `${sorted.indexOf()}/${arr.lenght}`
   }
 }
 
